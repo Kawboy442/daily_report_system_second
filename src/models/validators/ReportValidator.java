46f9464945a27ce3203b70907adacd0ee4e87af4
ryosuke.ito <kawboy442@gmail.com>
2021-01-24 16:50:53 +0900

    feat: 日報のバリデーションに作成済みかどうかを判定するメソッドを設定

--- src/models/validators/ReportValidator.java
+++ src/models/validators/ReportValidator.java
@@ -1,13 +1,22 @@
 package models.validators;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
 import models.Report;
+import utils.DBUtil;
 
 public class ReportValidator {
-	public static List<String> validate(Report r) {
+	public static List<String> validate(Report r, Boolean reportDateDuplicateCheckFlag) {
 		List<String> errors = new ArrayList<String>();
+
+		String reportDate_error = _validateReportDate(r.getReport_date(), reportDateDuplicateCheckFlag);
+		if(!reportDate_error.equals("")) {
+			errors.add(reportDate_error);
+		}
 
 		String title_error = _validateTitle(r.getTitle());
 		if(!title_error.equals("")) {
@@ -33,6 +42,21 @@ public class ReportValidator {
 	private static String _validateContent(String content) {
 		if(content == null || content.equals("")) {
 			return "内容を入力してください。";
+		}
+
+		return "";
+	}
+
+	private static String _validateReportDate(Date reportDate, Boolean reportDateDuplicateCheckFlag) {
+
+		// 日報が重複していないかチェック
+		if(reportDateDuplicateCheckFlag) {
+			EntityManager em = DBUtil.createEntityManager();
+			long report_date_count = (long) em.createNamedQuery("checkReportDate", Long.class);
+			em.close();
+			if(report_date_count > 0) {
+				return "すでに日報が作成されています。";
+			}
 		}
 
 		return "";
