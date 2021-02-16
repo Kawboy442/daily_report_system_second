5cea7f8108b11469b46ce92b4baea36ffeda2ec6
R.Ito <kawboy442@gmail.com>
2021-02-16 22:35:06 +0900

    remove: 不要になったバリデーションを除去

--- src/models/validators/ReportValidator.java
+++ src/models/validators/ReportValidator.java
@@ -1,8 +1,6 @@
 package models.validators;
 
 import java.sql.Date;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,16 +21,6 @@ public class ReportValidator {
 		String content_error = _validateContent(r.getContent());
 		if(!content_error.equals("")) {
 			errors.add(content_error);
-		}
-
-		String startTime_error = _validateStartTime(r.getStart_time(),r.getReport_date());
-		if(!startTime_error.equals("")) {
-			errors.add(startTime_error);
-		}
-
-		String endTime_error = _validateEndTime(r.getEnd_time(),r.getReport_date());
-		if(!endTime_error.equals("")) {
-			errors.add(endTime_error);
 		}
 
 		String reportDate_error = _validateReportDate(r.getReport_date(), reportDateDuplicateCheckFlag);
@@ -56,26 +44,6 @@ public class ReportValidator {
 			return "内容を入力してください。";
 		}
 
-		return "";
-	}
-
-	private static String _validateStartTime(Timestamp startTimestamp, Date report_date) {
-		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
-		String startTime = fmt.format(startTimestamp);
-		String reportDate = fmt.format(report_date);
-		if(!(startTime.equals(reportDate))) {
-			return "始業時間の日付が日報の日付と異なっています。";
-		}
-		return "";
-	}
-
-	private static String _validateEndTime(Timestamp endTimestamp, Date report_date) {
-		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
-		String endTime = fmt.format(endTimestamp);
-		String reportDate = fmt.format(report_date);
-		if(!(endTime.equals(reportDate))) {
-			return "終業時間の日付が日報の日付と異なっています。";
-		}
 		return "";
 	}
 
